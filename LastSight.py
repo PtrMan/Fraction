c75df1a85304a2e1e6d25fac536d6f08267067b3
patrick <patrickhammer9@hotmail.com>
2014-01-27 06:32:24 +0000

    video version

--- LastSight.py
+++ LastSight.py
@@ -1,3 +1,4 @@
-Mem=[[] for i in range(10)] #history 10
-Assoc={}
-Extracted=[]
+Mem=[[] for i in range(10)] #history 10 raw memory
+Assoc={}                    #names given by the AI
+Extracted=[]                #rules the AI extracted
+
